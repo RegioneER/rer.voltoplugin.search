c9d9d8754ad0616f4431519dc680fff35a9c54cb
Andrea Cecchi <andrea.cecchi85@gmail.com>
2025-02-17 17:31:22 +0100

    isort

--- src/rer/voltoplugin/search/setuphandlers.py
+++ src/rer/voltoplugin/search/setuphandlers.py
@@ -1,8 +1,8 @@
-from Products.CMFPlone.interfaces import INonInstallable
-from zope.interface import implementer
 from plone.registry.interfaces import IRegistry
+from Products.CMFPlone.interfaces import INonInstallable
 from Products.CMFPlone.interfaces import ISearchSchema
 from zope.component import getUtility
+from zope.interface import implementer
 
 
 @implementer(INonInstallable)
